68243e20aedb570e97f6f6d8ee5e553d6cc3ad6c
Benjamin Carson <benjohncarson@gmail.com>
2016-12-07 16:40:44 +0000

    Minor refactoring

--- passphrases.rb
+++ passphrases.rb
@@ -1,13 +1,13 @@
 def play_pass(str, n)
-	string = letter_shift(str, n)
+	letters = ("a".."z").to_a
+
+	string = letter_shift(str, n, letters)
 	string = nine_comp(string)
-	string = change_case(string)
+	string = change_case(string, letters)
 	string.reverse
 end
 
-def letter_shift(str, n)
-	letters = ("a".."z").to_a
-
+def letter_shift(str, n, letters)
 	str.chars.map do |char|
 		letters.include?(char.downcase) ? letters[(letters.index(char.downcase) + n) - 26] : char
 	end.join
@@ -19,9 +19,7 @@ def nine_comp(str)
 	str.chars.map { |char| numbers.include?(char) ? (9 - char.to_i) : char}.join
 end
 
-def change_case(str)
-	letters = ("a".."z").to_a
-
+def change_case(str, letters)
 	str.chars.map.with_index do |char, i|
 		letters.include?(char) ? (i % 2 == 0 ? char.upcase : char) : char
 	end.join
