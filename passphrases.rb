5f28bfc51d959cddacb595a0ea6fee990a00e34b
Benjamin Carson <benjohncarson@gmail.com>
2016-12-07 16:28:42 +0000

    First working attempt

--- passphrases.rb
+++ passphrases.rb
@@ -1,13 +1,15 @@
 def play_pass(str, n)
 	string = letter_shift(str, n)
 	string = nine_comp(string)
+	string = change_case(string)
+	string.reverse
 end
 
 def letter_shift(str, n)
 	letters = ("a".."z").to_a
 
 	str.chars.map do |char|
-		letters.include?(char.downcase) ? letters[(letters.index(char) + n) - 26] : char
+		letters.include?(char.downcase) ? letters[(letters.index(char.downcase) + n) - 26] : char
 	end.join
 end
 
@@ -17,4 +19,12 @@ def nine_comp(str)
 	str.chars.map { |char| numbers.include?(char) ? (9 - char.to_i) : char}.join
 end
 
-puts play_pass("hel5loz", 2)
+def change_case(str)
+	letters = ("a".."z").to_a
+
+	str.chars.map.with_index do |char, i|
+		letters.include?(char) ? (i % 2 == 0 ? char.upcase : char) : char
+	end.join
+end
+
+puts play_pass("hEl5 loz", 2)
