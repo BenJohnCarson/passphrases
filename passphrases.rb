099a85d0eca86300eb817d1d944a0c617cc980b0
Benjamin Carson <benjohncarson@gmail.com>
2016-12-07 16:05:00 +0000

    Created nine_comp method

--- passphrases.rb
+++ passphrases.rb
@@ -1,5 +1,6 @@
 def play_pass(str, n)
-	letter_shift(str, n)
+	string = letter_shift(str, n)
+	string = nine_comp(string)
 end
 
 def letter_shift(str, n)
@@ -8,6 +9,12 @@ def letter_shift(str, n)
 	str.chars.map do |char|
 		letters.include?(char.downcase) ? letters[(letters.index(char) + n) - 26] : char
 	end.join
+end
+
+def nine_comp(str)
+	numbers = ("0".."9").to_a
+
+	str.chars.map { |char| numbers.include?(char) ? (9 - char.to_i) : char}.join
 end
 
 puts play_pass("hel5loz", 2)
